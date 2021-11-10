3b08150ca284f95ce27f03c133fc1144e0fe05d1
Jake <jakerfilut@gmail.com>
2021-11-10 12:13:05 -0700

    step 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 // Your code here
+function renderAnimal(animal) {
+    let animalcard = document.createElement("span")
+    animalcard.innerHTML = `
+    <img src="${animal.image}"/>
+    <div class = "content">
+    <h2>${animal.name}</h2>
+    <p>
+    Votes: ${animal.votes}
+    </div>`
+    let charBar = document.getElementById("character-bar")
+    charBar.append(animalcard)       
+    
+    animalcard.addEventListener('click', movePicture)
+
+    function movePicture(){
+        let detailedInfo = document.getElementById("#detailed-info")
+        detailedInfo.innerHTML = `<img src="${animal.image}"/>`
+    }
+}
+
+
+
+
+function fetchAnimal() {
+    const URL = "http://localhost:3000"
+    fetch(URL + `/characters/`)
+        .then(res => res.json())
+        .then(animalData => animalData.forEach(animal => renderAnimal(animal)))
+    
+}
+
+function intialize() {
+    fetchAnimal()
+}
+
+intialize()
+
+// function movePicture(){
+//     let newImage = document.getElementById("image")
+//     newImage.innerHTML = `<img src="${animal.image}"/>`
+// }
